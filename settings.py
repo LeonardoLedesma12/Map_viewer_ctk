8f735fe468cc895526f33b2fb456778347c92cd6
Leonardo Ledesma <leonardoledesma@MacBook-Air-de-Leonardo.local>
2023-06-25 21:48:48 -0600

    full Map viewer app

---
+++ settings.py
@@ -0,0 +1,23 @@
+#map urls
+MAIN_URL = 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+TERRAIN_URL = 'https://mt0.google.com/vt/lyrs=s&h1=en&x={x}&y={y}&z={z}&s=Ga'
+PAINT_URL = "http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png"
+
+#colors
+COLOR_RANGE = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+ENTRY_BG = '#FFF'
+SIDE_PANEL_BG = '#EEE'
+HISTORY_HOVER_COLOR = '#AAA'
+BUTTON_COLOR = '#DBDBDB'
+BUTTON_HOVER_COLOR = '#C9C9C9'
+
+#text
+TEXT_FONT = 'Helvetica'
+TEXT_SIZE = 14
+TEXT_COLOR = '#000'
+CLOSE_ITEM_COLOR = '#555'
+
+#image paths
+MAP_IMAGE_PATH = 'images/map.png'
+PAINT_IMAGE_PATH = 'images/paint.png'
+TERRAIN_IMAGE_PATH = 'images/terrain.png'
